3baceaf695606d13f6bf3e9cf1fb36ab7dd6ddbf
Thomas Schreiber <tom@insatsu.us>
2011-09-30 14:24:33 +0200

    add biblion.utils to setup.py

--- setup.py
+++ setup.py
@@ -3,34 +3,26 @@ from distutils.core import setup
 # see requirements.txt for dependencies
 
 
-
 setup(
-    name = "biblion",
-    version = "0.1.dev11",
-    author = "Eldarion",
-    author_email = "development@eldarion.com",
-    description = "the eldarion.com blog app intended to be suitable for site-level company and project blogs",
-    long_description = open("README.rst").read(),
-    license = "BSD",
-    url = "http://github.com/eldarion/biblion",
-    packages = [
+    name="biblion",
+    version="0.1.dev11",
+    author="Eldarion",
+    author_email="development@eldarion.com",
+    description="the eldarion.com blog app intended to be suitable for site-level company and project blogs",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="http://github.com/eldarion/biblion",
+    packages=[
         "biblion",
         "biblion.templatetags",
         "biblion.utils",
     ],
-    package_data = {
+    package_data={
         "biblion": [
             "templates/biblion/*.xml",
         ]
     },
-    install_requires=[
-        "creole==1.2",
-        "docutils==0.7",
-        "markdown==2.0.3",
-        "Pygments==1.4",
-        "textile==2.1.4",
-    ],
-    classifiers = [
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
